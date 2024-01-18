c101f76789bd15f9f0413f4c0ec2b83d51da7e8f
Bohdan Popovych <flecks07@gmail.com>
2024-01-18 19:28:59 +0200

    Show a modal with classes

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
-      {
-        open && <div className="overlay">
+
+      <div className={`overlay animated ${open ? 'show' : ''}`}>
           <div className="modal">
             <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
               <title />
@@ -17,7 +17,7 @@ function App() {
             <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
           </div>
         </div>
-      }
+
     </div>
   );
 }
